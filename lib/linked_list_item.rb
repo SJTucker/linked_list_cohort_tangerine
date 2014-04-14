7d35cdb5ab946ee7c6f3cda09d5313f4bc408767
Sam Tucker <samueljeffersontucker@gmail.com>
2014-04-14 09:39:49 -0500

    final commit

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,5 +1,36 @@
 class LinkedListItem
-  def initialize(item)
+  attr_accessor :payload
+  attr_accessor :next_list_item
 
+  def initialize(payload)
+    @payload = payload
+  end
+
+  def next_list_item= val
+    raise ArgumentError if val == self
+    @next_list_item= val
+  end
+
+  def last?
+    @next_list_item == nil ? true : false
+  end
+
+  include Comparable
+  def ===(another)
+    self.object_id === another.object_id ? true : false
+  end
+
+  def <=> (other)
+    #self.payload.to_s <=> other.payload.to_s
+
+    first = self.payload.to_s
+    second = other.payload.to_s
+    if (self.payload.class == Symbol && other.payload.class != Symbol)
+      return 1
+    elsif (other.payload.class == Symbol && self.payload.class != Symbol)
+      return -1
+    else
+      return first <=> second
+    end
   end
 end
